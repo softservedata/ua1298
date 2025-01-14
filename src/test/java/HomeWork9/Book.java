5723e4bae15a4369afc009c114f51e84302243e1
Levkovych Yuliia <levjuli98@gmail.com>
2025-01-14 01:41:31 +0100

    HomeWork9Task3

---
+++ src/test/java/HomeWork9/Book.java
@@ -0,0 +1,52 @@
+package HomeWork9;
+
+public class Book {
+    private String title;
+    private String author;
+    private String genre;
+    private int year;
+
+    public Book(String title, String author, String genre, int year) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Book{title='%s', author='%s', genre='%s', year=%d}", title, author, genre, year);
+    }
+}
