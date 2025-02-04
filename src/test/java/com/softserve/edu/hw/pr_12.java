e8e70299275588aead39c294ba510528b1375ad3
roman <roman270794@gmail.com>
2025-02-04 13:54:02 +0200

    examples

---
+++ src/test/java/com/softserve/edu/hw/pr_12.java
@@ -0,0 +1,62 @@
+package com.softserve.edu.hw;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class pr_12 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "tester0202@gmail.com, Qwerty123!"
+    })
+    @DisplayName("Verify loginning")
+    void testUserLoginning(String email, String pass) throws InterruptedException {
+        driver.get("http://localhost:4205/#/ubs");
+
+        WebElement signUpButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".ubs-header-sign-in")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", signUpButton);
+
+        WebElement emailField = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("email")));
+        emailField.sendKeys(email);
+
+        WebElement passwordField = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("password")));
+        passwordField.sendKeys(pass);
+
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".ubsStyle")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", submitButton);
+
+        WebElement headerUser = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a.ubs-header_user-name")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", headerUser);
+
+        WebElement signOut = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".drop-down-item")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", signOut);
+    }
+}
