4ad4c54b77fb9de6df045dd623d7b687534b9084
Anatolii <you@example.com>
2025-02-16 14:49:20 +0200

    Added screen taking and extended test runner

--- HWTestFramework/src/test/java/com/softserve/edu/framework/tests/TestRunner.java
+++ HWTestFramework/src/test/java/com/softserve/edu/framework/tests/TestRunner.java
@@ -3,9 +3,13 @@ package com.softserve.edu.framework.tests;
 import com.softserve.edu.framework.lib.SignInUI;
 import com.softserve.edu.framework.lib.WebUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import io.github.cdimascio.dotenv.Dotenv;
@@ -14,9 +18,18 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
+@ExtendWith(TestResultExtension.class)
 public class TestRunner {
     @FindBy(css = ".header_sign-in-link.tertiary-global-button")
     protected WebElement signInButton;
@@ -67,6 +80,7 @@ public class TestRunner {
     protected static Dotenv dotenv;
     protected static WebUtils webUtils;
     protected static SignInUI signInUI;
+    private static final String TIME_TEMPLATE = "yyyy-MM-dd_HH-mm-ss-S";
 
 
     private static void initializeBrowser(){
@@ -96,6 +110,35 @@ public class TestRunner {
         dotenv = Dotenv.configure().directory("target/test-classes").load();
     }
 
+    private void takeScreenShot() {
+        LocalDateTime localDate = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(TIME_TEMPLATE);
+        String currentTime = localDate.format(formatter);
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(scrFile, new File("./" + currentTime + "_screenshot.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void takePageSource() {
+        //String currentTime = new SimpleDateFormat(TIME_TEMPLATE).format(new Date());
+        //
+        LocalDateTime localDate = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(TIME_TEMPLATE);
+        String currentTime = localDate.format(formatter);
+        //
+        String pageSource = driver.getPageSource();
+        byte[] strToBytes = pageSource.getBytes();
+        Path path = Paths.get("./" + currentTime + "_" + "_source.html.txt");
+        try {
+            Files.write(path, strToBytes, StandardOpenOption.CREATE);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @BeforeAll
     static void setup(){
         setEnv();
@@ -118,6 +161,8 @@ public class TestRunner {
     public void tearThis(TestInfo testInfo) {
         if (!isTestSuccessful) {
             logger.error(() -> "Test_Name = " + testInfo.getTestMethod().toString() + " failed");
+            takeScreenShot();
+            takePageSource();
         } else {
             logger.info(() -> "Test " + testInfo.getTestMethod() + " done.");
         }
